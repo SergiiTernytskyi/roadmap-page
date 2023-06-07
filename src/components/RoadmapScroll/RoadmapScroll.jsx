db9b7fb2160f94d4262ec4a2730755d1261a0d85
Sergii Ternytskyi <serhij86@gmail.com>
2023-06-08 00:26:45 +0300

    data refactoring

--- src/components/RoadmapScroll/RoadmapScroll.jsx
+++ src/components/RoadmapScroll/RoadmapScroll.jsx
@@ -1,27 +1,7 @@
+import { phasesList } from "../../data/phasesList";
 import { PhaseCard } from "../PhaseCard/PhaseCard";
 import { Title } from "../Title/Title";
 import { Card, ListStyled } from "./RoadmapScroll.styles";
-
-const phasesList = [
-  {
-    name: "Phase 1",
-    list: [
-      "Special events",
-      "Launch social networks",
-      "Launch token",
-      "Launch token on DEX",
-    ],
-  },
-
-  {
-    name: "Phase 2",
-    list: ["Launch on token CEX", "Burn 1% PAND", "Staking"],
-  },
-  {
-    name: "Phase 3",
-    list: ["Take over the meme-coin market"],
-  },
-];
 
 export const RoadmapScroll = () => {
   return (
---
+++ src/data/phasesList.js
@@ -0,0 +1,20 @@
+export const phasesList = [
+  {
+    name: "Phase 1",
+    list: [
+      "Special events",
+      "Launch social networks",
+      "Launch token",
+      "Launch token on DEX",
+    ],
+  },
+
+  {
+    name: "Phase 2",
+    list: ["Launch on token CEX", "Burn 1% PAND", "Staking"],
+  },
+  {
+    name: "Phase 3",
+    list: ["Take over the meme-coin market"],
+  },
+];
